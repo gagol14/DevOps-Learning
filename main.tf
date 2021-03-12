7326bb8fd6f366e226affa2f1eb533cab7ca65b1
gagol14 <32977937+gagol14@users.noreply.github.com>
2021-03-12 20:46:35 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,36 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+#Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
+
+#Storage account
+resource "azurerm_storage_account" "StorAcc1" {
+  name = "hwnafvab19962021bubbub"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  account_kind = "StorageV2"
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+}
+
+#Container
+resource "azurerm_storage_container" "Cont1" {
+  name = "iso-folder"
+  storage_account_name = azurerm_storage_account.StorAcc1.name
+  container_access_type = "private"
+}
